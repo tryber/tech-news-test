c67c761310643a67ee0c5b69a5069e4bab7fddee
Euclécio Josias Rodrigues <eucjosias@gmail.com>
2020-09-22 09:18:58 -0300

    Importa json

--- tech_news_data_collector/news_importer.py
+++ tech_news_data_collector/news_importer.py
@@ -1,9 +1,10 @@
 import json
 
-
 def csv_importer():
     return NotImplementedError
 
-
 def json_importer(file_path):
-    return NotImplementedError
+    with open(file_path, 'r') as read_file:
+        news = json.load(read_file)
+
+    return news
